cbaab4ddf7345a6d53be97bb5b5d7abe61adfcde
dev-sumon <146063221+dev-sumon@users.noreply.github.com>
2025-05-08 12:55:27 +0600

    some update

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\Auth\GoogleController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::get('auth/google', [GoogleController::class,'redirectToGoogle'])->name('redirect.google');
+Route::get('auth/google/callback', [GoogleController::class,'handleGoogleCallback']);
